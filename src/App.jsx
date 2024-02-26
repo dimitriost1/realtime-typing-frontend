fa1164693f22188c91dbc5ad9b88caa96b4d3aea
dimitriost1 <dtsolakid@gmail.com>
2024-02-26 23:38:42 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react"
+
+function App() {
+
+  const [text, setText] = useState("")
+
+  return (
+    <>
+      <div className="lg:columns-2 my-4">
+        <div className="mockup-window border bg-base-300">
+          <textarea className="textarea textarea-primary textarea-lg h-80" placeholder="Bio" onChange={(e) => setText(e.target.value)} value={text} />
+        </div>
+        <div className="mockup-window border bg-base-300 ">
+          <textarea className="textarea textarea-primary textarea-lg h-80" placeholder="Bio" disabled value={text} />
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
